982d2b08988de1a6b7889eb8b487f3fede4d1a04
ojalex <jumaalex448@gmail.com>
2025-06-28 11:12:53 +0300

    W01 Assignment: C# Programming Excercises

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,84 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //C# Programming Exercise 3: Loops by Alex Juma
+        //###############################################
+
+        //ASSIGNMEN
+        //######################################################################
+
+        //generating the random number once.
+        Random randNumber = new Random();
+        int magicNumber = randNumber.Next(1, 101);
+        int yourGuess = -1; //set initial guess to -1 which is out of range to enable the loop start from begining
+        string text = "";
+        int numberOfGuess = 0;
+
+        while (yourGuess != magicNumber)
+        {
+            Console.Write($"What is your guess? ");
+            string guess = Console.ReadLine();
+            yourGuess = int.Parse(guess);
+
+            numberOfGuess = numberOfGuess + 1; //Count number of attempts
+
+            if (yourGuess < magicNumber)
+            {
+                text = "Lower";
+            }
+            else if (yourGuess > magicNumber)
+            {
+                text = "Higher";
+            }
+            else
+            {
+                text = $"You guessed it! after {numberOfGuess} trials.";
+            }
+
+            Console.WriteLine($"{text}");
+            Console.WriteLine();
+        }
+
     }
 }
+
+        
+        
+        
+        
+        //MY NOTES FROM THE TOPIC 
+        //Creating a List
+        //--------------------------------
+        //List<string> colors= new List<string>();
+        //colors.Add("Red");
+        //colors.Add("Yellow");
+        // colors.Add("Blue");
+
+        //foreach loop
+        //--------------------------------
+        //foreach (string color in colors)
+        //{
+        //    Console.WriteLine(color);
+        //}
+
+        //for loop
+        //----------------------------------
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.Write(i);
+        //}
+        //string response;
+
+        //do while
+        //-----------------------
+        //do
+        //{
+        //    Console.WriteLine("Do you want to continue? ");
+        //    response = Console.ReadLine();
+        //} 
+        //while (response == "yes");
+
+        //Rando Number  generation
+        //------------------------
+        //Random randomGenerator = new Random();
+        //int number = randomGenerator.Next(1, 11);
